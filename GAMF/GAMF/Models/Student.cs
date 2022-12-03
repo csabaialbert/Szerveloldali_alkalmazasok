fc0df9f1a3b203a06b9692229ca30c126f96930f
csabaialbert <68478226+csabaialbert@users.noreply.github.com>
2022-12-03 22:25:55 +0100

    G1

---
+++ GAMF/GAMF/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GAMF.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        [Display(Name = "Családnév")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Keresztnév")]
+        public string FirstMidName { get; set; }
+
+        [Display(Name = "Első jelentkezés")]
+        public DateTime EnrollmentDate { get; set; }
+
+        [Display(Name = "Jelentkezések")]
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+
+    }
+
+}
