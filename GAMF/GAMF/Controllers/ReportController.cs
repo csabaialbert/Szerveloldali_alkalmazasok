2c21bed92e0142e4275aa88d9e287ffb53abe6d7
csabaialbert <68478226+csabaialbert@users.noreply.github.com>
2022-12-04 14:57:13 +0100

    Student credit report

--- GAMF/GAMF/Controllers/ReportController.cs
+++ GAMF/GAMF/Controllers/ReportController.cs
@@ -1,6 +1,7 @@
 ﻿using GAMF.Data;
 using GAMF.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace GAMF_N.Controllers
 {
@@ -24,6 +25,44 @@ namespace GAMF_N.Controllers
 
 
             return View(result.ToList());
+        }
+        public class k
+        {
+            public int id;
+            public int cr;
+        }
+        public IActionResult StudentCredit()
+        {
+            var list = new List<StudentsCreditVM>();
+            int valami = 0;
+            int cred = 0;
+
+            var st = _Context.Students.ToList();
+            var en = _Context.Enrollments.ToList();
+            var co = _Context.Courses.ToList();
+            foreach (var item in st)
+            {
+                var res = new StudentsCreditVM();
+                res.Id = item.Id;
+                res.FirstMidName = item.LastName;
+                res.LastName = item.LastName;
+                res.kreditszam = 0;
+                foreach (var item1 in en)
+                {
+                    if (item.Id == item1.StudentId)
+                    {
+                        foreach (var item2 in co)
+                        {
+                            if (item1.CourseId == item2.CourseId)
+                            {
+                                res.kreditszam += item2.Credits;
+                            }
+                        }
+                    }
+                }
+                list.Add(res);
+            }
+            return View(list);
         }
     }
 }
--- GAMF/GAMF/Models/Student.cs
+++ GAMF/GAMF/Models/Student.cs
@@ -6,12 +6,15 @@ namespace GAMF.Models
     {
         public int Id { get; set; }
         [Display(Name = "Családnév")]
+        [Required(ErrorMessage = "Családnév kitöltése kötelező!")]
         public string LastName { get; set; }
 
         [Display(Name = "Keresztnév")]
+        [Required(ErrorMessage = "Keresztnév kitöltése kötelező!")]
         public string FirstMidName { get; set; }
 
         [Display(Name = "Első jelentkezés")]
+        [Required(ErrorMessage = "Első jelentkezés kitöltése kötelező!")]
         public DateTime EnrollmentDate { get; set; }
 
         [Display(Name = "Jelentkezések")]
